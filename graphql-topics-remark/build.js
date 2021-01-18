885793e5eeda6d1ad8f70d3e994a32415252c5cb
Cameron Manderson <cam@alpaca.travel>
2021-01-18 15:33:10 +1100

    Creates a method to create markdown composed from the graphql examples

---
+++ graphql-topics-remark/build.js
@@ -0,0 +1,38 @@
+const unified = require("unified");
+const visit = require("unist-util-visit");
+const createStream = require("unified-stream");
+const parse = require("remark-parse");
+const stringify = require("remark-stringify");
+const shortcodes = require("remark-shortcodes");
+const path = require("path");
+const fs = require("fs");
+
+// Replaces a shortcode
+function attacher() {
+  return transformer;
+
+  function transformer(tree, file) {
+    visit(tree, "shortcode", function (node, index, parent) {
+      if (node.identifier === "CodeBlock") {
+        const { src } = node.attributes;
+        const resolved = path.resolve(__dirname, "../graphql", src);
+        const contents = fs.readFileSync(resolved, "utf-8");
+
+        parent.children.splice(index, 1, {
+          type: "code",
+          lang: "graphql",
+          value: contents,
+        });
+      }
+    });
+  }
+}
+
+const processor = unified()
+  .use(parse)
+  .use(shortcodes)
+  .use(attacher)
+  .use(stringify);
+
+// Parse the command input and output the markdown substituted
+process.stdin.pipe(createStream(processor)).pipe(process.stdout);
---
+++ graphql-topics/itinerary/create-a-basic-favourites-list.md
@@ -0,0 +1,5 @@
+# Create a basic favourites list
+
+To get started, create an itinerary
+
+[[CodeBlock src="itinerary/CreateItinerary.graphql"]]
