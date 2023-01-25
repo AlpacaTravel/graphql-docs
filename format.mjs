1b7635c4b68c5958039bb17def8f8e74546d2804
Cameron Manderson <cam@alpaca.travel>
2023-01-25 13:52:47 +1100

    Adds in a formatting option to create TOC's and expand for other markdown insertions

--- format.mjs
+++ format.mjs
@@ -1,19 +1,29 @@
-const fs = require("fs");
-const path = require("path");
-const remark = require("remark");
-const remove = require("unist-util-remove");
-const lz = require("lz-string");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import remark from "remark";
+import remove from "unist-util-remove";
+import visit from "unist-util-visit";
+import lz from "lz-string";
+import prettier from "remark-prettier";
+import toc from "remark-toc";
+import consistent from "remark-preset-lint-consistent";
+import recommended from "remark-preset-lint-recommended";
 
 const [, , fileReference] = process.argv;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const studio = `https://studio.apollographql.com/sandbox/explorer`;
 const endpoint = `https://withalpaca.com/api/graphql?accessToken=UPDATE_TOKEN`;
 
 function removeExisting() {
   return (tree) => {
+    console.log(tree);
+
     remove(tree, (node) => {
       const [first] = node.children || [];
-      if (first && /^View/.test(first.value)) {
+      if (first && /^Link to/.test(first.value)) {
         return true;
       }
 
@@ -23,13 +33,33 @@ function removeExisting() {
 }
 
 function link(doc) {
-  const explorerURLState = lz.compressToBase64(
+  const explorerURLState = lz.compressToEncodedURIComponent(
     JSON.stringify({ document: doc })
   );
 
   return `${studio}?endpoint=${encodeURIComponent(
     endpoint
   )}&explorerURLState=${explorerURLState}`;
+}
+
+function tightenLists() {
+  return (tree) => {
+    visit(tree, "list", (node) => {
+      node.loose = false;
+    });
+
+    visit(tree, "listItem", (node) => {
+      node.loose = false;
+    });
+  };
+}
+
+function debug() {
+  return (tree) => {
+    visit(tree, "listItem", (node) => {
+      console.log(node);
+    });
+  };
 }
 
 function addLink() {
@@ -43,7 +73,7 @@ function addLink() {
             children: [
               {
                 type: "text",
-                value: "View in ",
+                value: "Link to ",
               },
               {
                 type: "link",
@@ -72,8 +102,12 @@ async function main() {
 
   const file = await remark()
     .use(removeExisting)
-    .use(addLink)
+    // .use(addLink)
+    .use(consistent)
+    .use(recommended)
+    .use(toc)
+    .use(prettier)
     .process(fs.readFileSync(ref, "utf8"));
 
-  fs.writeFileSync(ref, file);
+  fs.writeFileSync(ref, file.toString("utf8"));
 }
